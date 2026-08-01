b25776e261a72488888a2c3fc99f13f16fc685e8
glowfi <86939591+glowfi@users.noreply.github.com>
2026-08-01 22:45:44 +0530

    add sample modules

---
+++ modules/time_date.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+dwm_time_date() {
+
+	printf "%s" "$SEP1"
+
+	# Print Output
+	output=$(date '+📅 %b %d %a %y  🕒 %I:%M%p')
+	printf "%s\n" "$output"
+
+	printf "%s" "$SEP2"
+}
+
+dwm_time_date
